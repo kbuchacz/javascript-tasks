9a7ae97d0f5d9ce44723c34baf29fab3e6bc05eb
Krzysztof Buchacz <k.buchacz@gmail.com>
2016-03-23 15:04:36 +0000

    Answers, part I

--- app/datasets.js
+++ app/datasets.js
@@ -1,32 +1,30 @@
 'use strict';
 
+var _ = require('lodash');
+
 module.exports = {
     countBy: function () {
-
-        return [];
+        return [[1, 2, 2, 6, 6, 7, 7, 7]];
     },
     countBy2: function () {
-
-        return [];
+        return [[3, 3, 4, 4, 4, 5]];
     },
     every: function () {
-
-        return [];
+        return [[{ALIVE: true}, {ALIVE: true}]];
     },
     every2: function () {
-
-        return [];
+        return [[true, true, true, true]];
     },
     filter: function () {
-
-        return [];
+        return [[{}, {}, {}, {name: 'Ali', age: 33, active: false},
+            {name: 'Kali', age: 21, active: true}], 'age'];
     },
     filter2: function () {
-
-        return [];
+        return [[{}, {}, {name: 'Bow', age: 23, active: true},
+            {name: 'Row', age: 23, active: true},
+            {name: 'Sou', age: 23, active: true}], 'active'];
     },
     find: function (arr) {
-
         return [];
     },
     find2: function () {
